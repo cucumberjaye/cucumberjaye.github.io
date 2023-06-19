ae6cd71821b03f3d3ef34e7f3c418fb070585734
Iskander <iskander.muzipov@gmail.com>
2023-06-19 17:38:39 +0300

    fix

--- webapp.js
+++ webapp.js
@@ -25,18 +25,17 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    document.getElementById("inner").style.display = "none";
-    document.getElementById("form").style.display = "display";
-    /*if (document.getElementById("form").style.display == "display") {
+    if (document.getElementById("form").style.isVisible) {
         let cardData = document.getElementById("cardData").value;
         let amount = document.getElementById("amount").value;
         tg.sendData({
             card_data: cardData,
             amount: amount,
         });
-    } else {*/
-   
-   // }
+    } else {
+        document.getElementById("inner").style.display = "none";
+        document.getElementById("form").style.display = "display";
+    }
 });
 
 
