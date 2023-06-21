fbf3f0a7ad4de8a1333d9660599f032e8efb1df4
Iskander <iskander.muzipov@gmail.com>
2023-06-21 12:16:47 +0300

    fix close

--- webapp.js
+++ webapp.js
@@ -27,7 +27,7 @@ for (let i = 0; i < buttons.length; i++) {
 tg.MainButton.setText("Next");
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    if (document.getElementById("form").style.isVisible) {
+    if (document.getElementById("form").style.display == "block") {
         let amount = document.getElementById("amount").value;
         tg.sendData(amount);
         tg.close();
