02d22d14a0df495fbedf39b6be65ee85697b27fa
Iskander <iskander.muzipov@gmail.com>
2023-06-21 17:29:25 +0300

    test

--- webapp.js
+++ webapp.js
@@ -23,7 +23,7 @@ for (let i = 0; i < buttons.length; i++) {
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
     if (document.getElementById("form").style.display == "block") {
         let amount = document.getElementById("amount").value;
-        document.getElementById("cardData").value = amount;
+        document.getElementById("cardData").innerText = amount;
        // tg.sendData(amount);
         //tg.close();
     } else {
